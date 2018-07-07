7cb810050b6c803a8e96bcca035932649c80fc8d
Brian Wright <briankw23@gmail.com>
2018-07-07 10:54:01 -0500

    setup and routing

---
+++ src/components/SingleOrder/SingleOrder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './SingleOrder.css';
+
+class SingleOdrer extends React.Component {
+  render () {
+    return (
+      <h1 className="SingleOrder">
+        Single Odrer
+      </h1>
+    );
+  }
+};
+
+export default SingleOdrer;
