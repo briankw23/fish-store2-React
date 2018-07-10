ca455ded07d05a55c58ed92dc63343b9bbbb022f
Brian Wright <briankw23@gmail.com>
2018-07-09 20:51:32 -0500

    orders by id are showing

--- src/components/OrderSpa/OrderSpa.js
+++ src/components/OrderSpa/OrderSpa.js
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import ordersRequests from '../../firebaseRequests/orders';
+import authRequests from '../../firebaseRequests/auth';
 import './OrderSpa.css';
 
 class OrderSpa extends React.Component {
+  state = {
+    orders: [],
+  }
+  componentDidMount () {
+    ordersRequests
+      .getRequest(authRequests.getUid())
+      .then((orders) => {
+        this.setState({ orders });
+      })
+      .catch((err) => {
+        console.error('error with orders get request', err);
+      });
+  }
+
   render () {
+    const orderComponenets = this.state.orders.map((order) => {
+      return (
+        <button>{order.id}</button>
+      );
+    });
     return (
-      <h1 className="OrderSpa">OrderSpa</h1>
+      <div className="OrderSpa">
+        <h2>Orders</h2>
+        <button>New Order</button>
+        <ul>
+          {orderComponenets}
+        </ul>
+      </div>
     );
   }
 };
