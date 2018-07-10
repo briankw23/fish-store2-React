f21075f453b6392560ae87220b6034f4386d28aa
Brian Wright <briankw23@gmail.com>
2018-07-09 21:02:03 -0500

    added routing

--- src/components/OrderSpa/OrderSpa.js
+++ src/components/OrderSpa/OrderSpa.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import ordersRequests from '../../firebaseRequests/orders';
 import authRequests from '../../firebaseRequests/auth';
 import './OrderSpa.css';
+import moment from 'moment';
 
 class OrderSpa extends React.Component {
   state = {
@@ -21,13 +23,21 @@ class OrderSpa extends React.Component {
   render () {
     const orderComponenets = this.state.orders.map((order) => {
       return (
-        <button>{order.id}</button>
+        <button
+          key= {order.id}
+          className= "col-xs-12 btn btn-default order-button"
+        >
+          <span className="col-xs-6">Order Number: {order.id}</span>
+          <span className="col-xs-6">Order Date: {moment(order.dateTime).format('LLL')}</span>
+        </button>
       );
     });
     return (
       <div className="OrderSpa">
         <h2>Orders</h2>
-        <button>New Order</button>
+        <button>
+          <Link to="/new">New Order</Link>
+        </button>
         <ul>
           {orderComponenets}
         </ul>
